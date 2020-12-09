59a1011db0c99ea2b15c0ebae4877595bb63daec
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-12-09 15:53:50 +0100

    project updated

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,17 +1,33 @@
 package home;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
+        //
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+        //
         primaryStage.setTitle("KeepTooDash");
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root));
--- src/home/controller/Controller.java
+++ src/home/controller/Controller.java
@@ -1,4 +1,96 @@
 package home.controller;
 
-public class Controller {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Button btnStudents;
+
+    @FXML
+    private Button btnTeachers;
+
+    @FXML
+    private Button btnFees;
+
+    @FXML
+    private Button btnUsers;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Pane pnlStatus;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private Label lblStatusMini;
+    @FXML
+    private GridPane pnFees;
+
+    @FXML
+    private GridPane pnUsers;
+
+    @FXML
+    private GridPane pnTeachers;
+
+    @FXML
+    private GridPane pnSettings;
+    @FXML
+    private GridPane pnStudents;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void handleClicks(ActionEvent event) {
+
+        if (event.getSource() == btnStudents) {
+            lblStatusMini.setText("/home/students");
+            lblStatus.setText("Students");
+            pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113,86,221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnStudents.toFront();
+        } else if(event.getSource() == btnTeachers) {
+            lblStatusMini.setText("/home/teachers");
+            lblStatus.setText("Teachers");
+            pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113,86,221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnTeachers.toFront();
+        }else if(event.getSource() == btnFees) {
+            lblStatusMini.setText("/home/fees");
+            lblStatus.setText("Fees");
+            pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113,86,221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnFees.toFront();
+        }else if(event.getSource() == btnUsers) {
+            lblStatusMini.setText("/home/users");
+            lblStatus.setText("Users");
+            pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113,86,221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnUsers.toFront();
+        }else if(event.getSource() == btnSettings) {
+            lblStatusMini.setText("/home/settings");
+            lblStatus.setText("Settings");
+            pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(113,86,221), CornerRadii.EMPTY, Insets.EMPTY)));
+            pnSettings.toFront();
+        }
+    }
+
+    @FXML
+    private void handleClose(ActionEvent event) {
+        Platform.exit();
+    }
 }
